76db1b9de4e1dc07af04de7d3ec19b7890a7b7bf
LucieKing1818 <kingl3@roehampton.ac.uk>
2026-02-08 16:59:56 +0000

    port changes
    fixed some port issues as wasn't loading docker properly

--- App/app.js
+++ App/app.js
@@ -12,7 +12,7 @@ const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.send("my name lucie king");
 });
 
 // Create a route for testing the db
