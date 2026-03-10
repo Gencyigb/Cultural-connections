05a344aef68aabbc060f9bad00fba7f416f32bcd
LucieKing1818 <kingl3@roehampton.ac.uk>
2026-03-10 15:42:42 +0000

    front page skeleton created

--- App/app.js
+++ App/app.js
@@ -4,45 +4,90 @@ const express = require("express");
 // Create express app
 var app = express();
 
-// Add static files location
+// ===== NEW: Configure PUG =====
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Add static files location (your existing static folder + new public folder)
 app.use(express.static("static"));
+app.use(express.static("public")); // NEW: for styles.css
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("my name lucie king");
+// ===== YOUR EXISTING ROUTES (KEEP THESE) =====
+
+// Create a route for root - UPDATED to use PUG
+app.get("/", function(req, res){
+  // Instead of res.send, use res.render with PUG
+  res.render('index', { 
+    title: 'Home' 
+  });
 });
 
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+app.get("/db_test", function(req, res){
+  sql = 'select * from test_table';
+  db.query(sql).then(results =>{
+    console.log(results);
+    res.send(results)
+  });
 });
 
 // Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
+app.get("/goodbye", function(req, res){
+  res.send("Goodbye world!");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+// Create a dynamic route for /hello/<name>
+app.get("/hello/:name", function(req, res){
+  console.log(req.params);
+  res.send("Hello " + req.params.name);
+});
+
+// ===== NEW ROUTES FOR SPRINT 3 =====
+
+// Categories page - YOUR PAGE
+app.get("/categories", function(req, res){
+  // Sample data - you'll replace this with database queries later
+  const categories = [
+    { name: 'Food', count: 156 },
+    { name: 'Language', count: 89 },
+    { name: 'Traditions', count: 124 },
+    { name: 'Festivals', count: 67 },
+    { name: 'Customs', count: 78 }
+  ];
+  
+  res.render('categories', {
+    title: 'Categories',
+    categories: categories
+  });
+});
+
+// Users list page
+app.get("/users", function(req, res){
+  // You can replace this with a real database query
+  const users = [
+    { name: 'Maria', country: 'Italy' },
+    { name: 'Takashi', country: 'Japan' },
+    { name: 'Amara', country: 'Nigeria' }
+  ];
+  
+  res.render('users', {
+    title: 'Community',
+    users: users
+  });
+});
+
+// Posts list page
+app.get("/posts", function(req, res){
+  res.render('posts', {
+    title: 'Posts'
+  });
 });
 
 // Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+app.listen(3000, function(){
+  console.log('Server running at http://127.0.0.1:3000/');
+  console.log('View your categories page at http://127.0.0.1:3000/categories');
 });
